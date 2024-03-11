4d38cbf67d822c4ac9ba51f259a7e616cdb29ba5
Sai Kumar Majji <saikumar170325@gmail.com>
2024-03-11 12:36:35 +0530

    added changes

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,21 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://127.0.0.1:27017/Users");
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+
+// mongoose.connect("mongodb://127.0.0.1:27017/Users");
 
 app.post("/login", (req, res) => {
   const { email, password } = req.body;
